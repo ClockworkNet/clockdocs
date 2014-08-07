2db714f26095bad0ee8128d0ea809190a456e599
Barrett Alexander <barrett@clockwork.net>
2014-08-07 13:45:18 -0500

    Fixed bitwise operator

--- app/scripts/random.js
+++ app/scripts/random.js
@@ -1,27 +1,28 @@
 /*global angular:false */
+/*jslint bitwise: true */
+
 'use strict';
 
 angular.module('Clockdoc.Utils')
 .service('Random', function() {
 	// Returns a random letter in [a-z]
 	this.letter = function() {
-		return ((11 + Math.random() * 26) || 0).toString(36);
+		return ((11 + Math.random() * 26) | 0).toString(36);
 	};
 
 	// Returns a random letter or number in [0-9a-z]
 	this.character = function() {
-		return ((1 + Math.random() * 36) || 0).toString(36);
+		return ((1 + Math.random() * 36) | 0).toString(36);
 	};
 
 	// Returns a random id
 	this.id = function(len) {
 		len = len || 16;
-		var s = [];
 		// The first character should be limited to a letter
-		s.push(this.letter());
+		var s = this.letter();
 		for (var i=1; i<len; i++) {
-			s.push(this.character());
+			s += this.character();
 		}
-		return s.join('');
+		return s;
 	};
 });
