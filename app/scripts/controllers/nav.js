ab26f1cd988f2d3cc1fc98a39d646f7581c32bcb
Barrett Alexander <barrett@clockwork.net>
2014-09-24 10:02:16 -0500

    Refactored RdCtrl to have sub-controls for more modularity

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -2,7 +2,7 @@
 'use strict';
 
 angular.module('Clockdoc.Controllers')
-.controller('RdCtrl', ['$scope', '$filter', '$timeout', '$http', '$q', 'FileSystem', 'Storage', 'Random', 'Svn', 'Scroll', 'Platform', 'Stylesheet', 'Ooxml', function($scope, $filter, $timeout, $http, $q, FileSystem, Storage, Random, Svn, Scroll, Platform, Stylesheet, Ooxml) {
+.controller('NavCtrl', ['$scope', '$filter', '$timeout', '$http', '$q', 'FileSystem', 'Storage', 'Svn', 'Platform', 'Stylesheet', 'Ooxml', function($scope, $filter, $timeout, $http, $q, FileSystem, Storage, Svn, Platform, Stylesheet, Ooxml) {
 
 	var EXTENSION = 'cw';
 	var ALERT_TIME = 5000;
@@ -88,8 +88,6 @@ angular.module('Clockdoc.Controllers')
 		}
 	};
 
-	$scope.rd = null;
-	$scope.sorting = false;
 	$scope.recentEntries = [];
 
 	Platform.load($scope, 'platform');
@@ -122,7 +120,7 @@ angular.module('Clockdoc.Controllers')
 		$scope.recentEntries = $scope.recentEntries.filter(function(r) {
 			return r.entryId !== entryId;
 		});
-		$scope.working = false;
+		$scope.setWorking(false);
 		$scope.result = null;
 		storage.set(RECENT_ENTRIES, $scope.recentEntries);
 		console.error('file error', e);
@@ -164,34 +162,6 @@ angular.module('Clockdoc.Controllers')
 			});
 	}
 
-	var flagTypes = $scope.flagTypes = [
-		{
-			type: 'definition',
-			name: 'Definition',
-			title: ''
-		},
-		{
-			type: 'dev_note',
-			name: 'Dev Note',
-			title: 'Dev Note'
-		},
-		{
-			type: 'open_question',
-			name: 'Open Question',
-			title: 'Open Question'
-		},
-		{
-			type: 'assumption',
-			name: 'Assumption',
-			title: 'Assumption'
-		}
-	];
-
-	$scope.availableTags = [
-		'Out of Scope',
-		'Future Phase'
-	];
-
 	function addFileStyle(result) {
 		Stylesheet.addRule(
 			'.' + result.id,
@@ -199,40 +169,12 @@ angular.module('Clockdoc.Controllers')
 		);
 	}
 
-	function getSampleDoc(type) {
-		if (type === 'rd') {
-			return {
-				title: 'Untitled Requirements Document',
-				author: 'Anonymous',
-				created: new Date(),
-				guid: Random.id(),
-				revisions: [$scope.exampleRevision()],
-				sections: [
-					createFeature('Definitions and Conventions'),
-					createFeature('Features'),
-					createFeature('Production'),
-					createFeature('Technology'),
-					createFeature('Security')
-				],
-				flags: []
-			};
-		}
-		return {
-			title: 'Untitled Requirements Document',
-			author: 'Anonymous',
-			created: new Date(),
-			guid: Random.id(),
-			revisions: [],
-			sections: []
-		};
-	}
-
 	function loadDoc(doc) {
-		$scope.rd = doc;
 		for (var id in doc.files) {
 			if (!doc.files.hasOwnProperty(id)) {continue;}
 			addFileStyle(doc.files[id]);
 		}
+		$scope.setRd(doc);
 	}
 
 	function flattenDoc(doc) {
@@ -296,101 +238,12 @@ angular.module('Clockdoc.Controllers')
 		$timeout(unwarn, ALERT_TIME);
 	}
 
-	function createFeature(title) {
-		title = title || 'Untitled';
-		var feature = {
-			'title': title,
-			'guid': Random.id(),
-			'content': '',
-			'features': [],
-			'flags': [],
-			'tags': []
-		};
-		return feature;
-	}
-
-	function createFlag(type) {
-		var flag = {};
-		flagTypes.some(function(f) {
-			if (f.type === type) {
-				flag = f;
-				return true;
-			}
-		});
-		return {
-			'title': flag.title,
-			'guid': Random.id(),
-			'type': flag.type,
-			'content': ''
-		};
-	}
-
-	// Creates a cache lookup to be used for getting 
-	function findItem(id, parent) {
-		if (!id) {return null;}
-		parent = parent || $scope.rd;
-
-		if (parent.guid === id) {
-			return {
-				index: 0,
-				item: parent
-			};
-		}
-
-		var checkChildren = function(a, type) {
-			if (!a) {return null;}
-			for (var i=0; i<a.length; i++) {
-				var o = a[i];
-				if (o.guid === id) {
-					return {
-						index: i,
-						item: o,
-						parent: {
-							item: parent,
-							type: type
-						}
-					};
-				}
-				var childItem = findItem(id, o);
-				if (childItem) {return childItem;}
-			}
-			return null;
-		};
-
-		var childTypes = ['features', 'sections'];
-
-		for (var i=0; i<childTypes.length; i++) {
-			var ct = childTypes[i];
-			var item = checkChildren(parent[ct], ct);
-			if (item) {return item;}
-		}
-
-		return null;
-	}
-
-	// Recursively access each feature in a section;
-	// Until the callback returns false
-	function eachItem(section, key, callback, level) {
-		if (!section[key] || !section[key].some) {return;}
-		level = level || 0;
-		section[key].some(function(f, i) {
-			var go = callback(f, i, section[key], level);
-			if (go === false) {
-				return true;
-			}
-			eachItem(f, key, callback, level + 1);
-		});
-	}
-
-	// The rd being edited
-	$scope.rd = null;
-
 	// Used to retain the result of a file open or SVN checkout
 	$scope.result = {};
 
 	/// Filesystem Methods ///
 	$scope.create = function() {
-		var doc = getSampleDoc('rd');
+		var doc = $scope.getSampleDoc('rd');
 		loadDoc(doc);
 		$scope.result = {};
 	};
@@ -399,7 +252,7 @@ angular.module('Clockdoc.Controllers')
 		var readResult = function(result) {
 			FileSystem.read(result)
 			.then(function(result) {
-				$scope.working = false;
+				$scope.setWorking(false);
 				if (!result || !result.content) {
 					return;
 				}
@@ -416,7 +269,7 @@ angular.module('Clockdoc.Controllers')
 		};
 
 		var onError = forgetEntry.bind(null, entryId);
-		$scope.working = true;
+		$scope.setWorking(true);
 		if (entryId) {
 			FileSystem.restore(entryId)
 			.then(readResult, onError);
@@ -440,21 +293,31 @@ angular.module('Clockdoc.Controllers')
 			.then(showMsg);
 	};
 
+	$scope.filename = function() {
+		if ($scope.result && $scope.result.entry) {
+			var name = $scope.result.entry.name;
+			return name.substring(0, name.lastIndexOf('.'));
+		}
+		return $scope.rd && $scope.rd.title;
+	};
+
 	$scope.saveAs = function() {
 		var rd = $scope.rd;
-		var name = $scope.result && $scope.result.entry ? $scope.result.entry.name : rd.title;
+		var name = $scope.filename();
 		var showMsg = warn.bind(this, 'Saved!', name, 'info');
 		var errMsg = warn.bind(this, 'Error!', name + ' could not be saved');
-		FileSystem.saveAs(rd.title, EXTENSION, angular.toJson(rd, true))
+		FileSystem.saveAs(name, EXTENSION, angular.toJson(rd, true))
 			.then(rememberEntry, errMsg)
 			.then(showMsg);
 	};
 
 	$scope.export = function(format) {
-		var name = $scope.result && $scope.result.entry ? $scope.result.entry.name : $scope.rd.title;
-		var showMsg = warn.bind(this, 'Done!', 'Your file has been exported', 'info');
-		var errMsg = warn.bind(this, 'Error!', name + ' could not be exported');
+		var name = $scope.filename();
 		var template = TEMPLATES[format];
+		var path = name + '.' + template.extension;
+
+		var showMsg = warn.bind(this, 'Done!', path + ' has been exported', 'info');
+		var errMsg = warn.bind(this, 'Error!', path + ' could not be exported');
 
 		var render = function(full) {
 			var output = template.transform(full, $scope.rd);
@@ -487,7 +350,7 @@ angular.module('Clockdoc.Controllers')
 	function svnRead(result) {
 		console.info('Read from SVN', result);
 
-		$scope.working = false;
+		$scope.setWorking(false);
 		$scope.result = result;
 		Svn.info(result.svnLocation.full)
 			.then(function(info) {
@@ -504,236 +367,42 @@ angular.module('Clockdoc.Controllers')
 	}
 
 	function svnCommitted(result) {
-		$scope.working = false;
+		$scope.setWorking(false);
 		$scope.result = result;
 		warn('Committed', 'changes committed to SVN', 'success');
 	}
 
 	$scope.openSvn = function() {
-		$scope.working = true;
+		$scope.setWorking(true);
 		Svn.open($scope.svn.url)
 			.then(svnRead, svnError);
 	};
 
 	$scope.checkout = function() {
-		$scope.working = true;
+		$scope.setWorking(true);
 		Svn.checkout($scope.svn.url)
 			.then(svnRead, svnError);
 	};
 
 	$scope.installSvn = function() {
-		$scope.working = true;
+		$scope.setWorking(true);
 		Svn.install().then(function() {
-			$scope.working = false;
+			$scope.setWorking(false);
 			$scope.svnInstalled = true;
 		}, function() {
 			// installation was cancelled
-			$scope.working = false;
+			$scope.setWorking(false);
 		});
 	};
 
 	$scope.commit = function() {
-		$scope.working = true;
+		$scope.setWorking(true);
 		$scope.result.content = angular.toJson($scope.rd, true);
 		Svn.commit($scope.result, $scope.svn.message)
 			.then(svnCommitted, svnError);
 	};
 
-	/// Section methods ///
-	$scope.insertSection = function(sectionIndex) {
-		var section = createFeature('Untitled Section');
-		$scope.rd.sections.splice(sectionIndex, 0, section);
-		Scroll.to(section.guid);
-	};
-
-	$scope.deleteSection = function(index) {
-		$scope.$apply(function() {
-			$scope.rd.sections.splice(index, 1);
-		});
-	};
-
-	/// Feature methods ///
-	$scope.deleteFeature = function(section, feature) {
-		eachItem(section, 'features', function(f, i, a) {
-			if (f.guid !== feature.guid) {
-				return true;
-			}
-			$scope.$apply(function() {
-				a.splice(i, 1);
-			});
-			return false;
-		});
-	};
-
-	$scope.insertFeature = function(features, featureIndex) {
-		if (!features) {
-			console.trace('Invalid features collection', arguments);
-			return warn('Error adding feature', 'The \'features\' collection was empty. You may have an invalid file.');
-		}
-		var feature = createFeature();
-		featureIndex = featureIndex || features.length + 1;
-		features.splice(featureIndex, 0, feature);
-
-		Scroll.to(feature.guid);
-	};
-
-	/// Tag methods ///
-	var featureHasTag = $scope.featureHasTag = function(feature, tag) {
-		return feature.tags && feature.tags.indexOf(tag) >= 0;
-	};
-
-	$scope.toggleTag = function(feature, tag) {
-		if (featureHasTag(feature, tag)) {
-			removeTag(feature, tag);
-		}
-		else {
-			addTag(feature, tag);
-		}
-	};
-
-	var addTag = $scope.addTag = function(feature, tag) {
-		if (!feature.tags) {
-			feature.tags = [];
-		}
-		feature.tags.push(tag);
-	};
-
-	var removeTag = $scope.removeTag = function(feature, tag) {
-		feature.tags = feature.tags.filter(function(t) {
-			return tag !== t;
-		});
-	};
-
-	/// Flag methods ///
-	$scope.insertFlag = function(flags, type) {
-		if (!flags) {
-			return warn('Error adding flag', 'The \'flags\' colleciton was empty.');
-		}
-		var flag = createFlag(type);
-		flags.push(flag);
-		Scroll.to(flag.guid);
-	};
-
-	$scope.deleteFlag = function(flags, index) {
-		$scope.$apply(function() {
-			flags.splice(index, 1);
-		});
-	};
-
-	$scope.moveItem = function(parentGuid, guid, newIndex) {
-		// Find the item being moved
-		var moved = findItem(guid);
-		if (!moved) {
-			console.error('Invalid moved item id', guid);
-			return;
-		}
-
-		// Get the containing object where the item was dropped
-		var parent = findItem(parentGuid || $scope.rd.guid);
-		if (!parent) {
-			console.error('Could not find drop parent in rd', parentGuid);
-			return;
-		}
-		var targetType = parent.item.guid === $scope.rd.guid ? 'sections' : 'features';
-
-		var swap = function() {
-			// Out with the old
-			moved.parent.item[moved.parent.type].splice(moved.index, 1);
-
-			// In with the new
-			parent.item[targetType].splice(newIndex, 0, moved.item);
-		};
-
-		$scope.$apply(swap);
-	};
-
-	$scope.moveFlag = function(flags, parentGuid, guid, newIndex) {
-		var currentIndex = -1;
-		var flag = null;
-		for (var i=0; i<flags.length; i++) {
-			if (flags[i].guid === guid) {
-				flag = flags[i];
-				currentIndex = i;
-				break;
-			}
-		}
-		if (currentIndex < 0) {
-			console.error('Invalid flag guid', guid);
-			return;
-		}
-		$scope.$apply(function() {
-			flags.splice(currentIndex, 1);
-			flags.splice(newIndex, 0, flag);
-		});
-	};
-
-	$scope.sortStart = function() {
-		$scope.$apply(function() {
-			$scope.sorting = true;
-		});
-	};
-
-	$scope.sortStop = function() {
-		$scope.$apply(function() {
-			$scope.sorting = false;
-		});
-	};
-
 	$scope.print = function() {
 		window.print();
-	};
-
-	$scope.addFile = function(result) {
-		if (!$scope.rd.files) {
-			$scope.rd.files = {};
-		}
-		result.id = Random.id();
-		$scope.rd.files[result.id] = result;
-		addFileStyle(result);
-	};
-
-	$scope.removeFile = function(id) {
-		if (!$scope.rd.files) {return;}
-		delete $scope.rd.files[id];
-	};
-
-	$scope.formatDate = function(d) {
-		return $filter('date')(d, 'yyyy-MM-dd');
-	};
-
-	$scope.getRevision = function() {
-		var revs = $scope.rd && $scope.rd.revisions;
-		if (!revs) {
-			return 0.0;
-		}
-		return revs[revs.length - 1].revision;
-	};
-
-	$scope.exampleRevision = function() {
-		var version = 1, revision = 0.1;
-		if ($scope.rd && $scope.rd.revisions) {
-			version = $scope.rd.revisions.length + 1;
-			revision = version / 10;
-		}
-		return {
-			'id': Random.id(),
-			'version': version,
-			'revision': revision,
-			'date': $scope.formatDate(new Date()),
-			'notes': '',
-			'author': ''
-		};
-	};
-
-	$scope.addRevision = function() {
-		if (!$scope.rd.revisions) {
-			$scope.rd.revisions = [];
-		}
-		$scope.rd.revisions.push($scope.exampleRevision());
-	};
-
-	$scope.removeRevision = function(i) {
-		if (!$scope.rd.revisions) {return;}
-		$scope.rd.revisions.splice(i, 1);
 	};
 }]);
---
+++ app/scripts/controllers/rd.js
@@ -0,0 +1,362 @@
+/*global angular:false */
+'use strict';
+
+angular.module('Clockdoc.Controllers')
+.controller('RdCtrl', ['$scope', '$filter', '$timeout', '$http', '$q', 'Random', 'Scroll', 'Platform', 'Stylesheet', function($scope, $filter, $timeout, $http, $q, Random, Scroll, Platform, Stylesheet) {
+
+	$scope.rd = null;
+	$scope.sorting = false;
+
+	Platform.load($scope, 'platform');
+
+	var flagTypes = $scope.flagTypes = [
+		{
+			type: 'definition',
+			name: 'Definition',
+			title: ''
+		},
+		{
+			type: 'dev_note',
+			name: 'Dev Note',
+			title: 'Dev Note'
+		},
+		{
+			type: 'open_question',
+			name: 'Open Question',
+			title: 'Open Question'
+		},
+		{
+			type: 'assumption',
+			name: 'Assumption',
+			title: 'Assumption'
+		}
+	];
+
+	$scope.availableTags = [
+		'Out of Scope',
+		'Future Phase'
+	];
+
+	function addFileStyle(result) {
+		Stylesheet.addRule(
+			'.' + result.id,
+			'background-image: url(' + result.data + ')'
+		);
+	}
+
+	function createFeature(title) {
+		title = title || 'Untitled';
+		var feature = {
+			'title': title,
+			'guid': Random.id(),
+			'content': '',
+			'features': [],
+			'flags': [],
+			'tags': []
+		};
+		return feature;
+	}
+
+	function createFlag(type) {
+		var flag = {};
+		flagTypes.some(function(f) {
+			if (f.type === type) {
+				flag = f;
+				return true;
+			}
+		});
+		return {
+			'title': flag.title,
+			'guid': Random.id(),
+			'type': flag.type,
+			'content': ''
+		};
+	}
+
+	// Creates a cache lookup to be used for getting 
+	function findItem(id, parent) {
+		if (!id) {return null;}
+		parent = parent || $scope.rd;
+
+		if (parent.guid === id) {
+			return {
+				index: 0,
+				item: parent
+			};
+		}
+
+		var checkChildren = function(a, type) {
+			if (!a) {return null;}
+			for (var i=0; i<a.length; i++) {
+				var o = a[i];
+				if (o.guid === id) {
+					return {
+						index: i,
+						item: o,
+						parent: {
+							item: parent,
+							type: type
+						}
+					};
+				}
+				var childItem = findItem(id, o);
+				if (childItem) {return childItem;}
+			}
+			return null;
+		};
+
+		var childTypes = ['features', 'sections'];
+
+		for (var i=0; i<childTypes.length; i++) {
+			var ct = childTypes[i];
+			var item = checkChildren(parent[ct], ct);
+			if (item) {return item;}
+		}
+
+		return null;
+	}
+
+	// Recursively access each feature in a section;
+	// Until the callback returns false
+	function eachItem(section, key, callback, level) {
+		if (!section[key] || !section[key].some) {return;}
+		level = level || 0;
+		section[key].some(function(f, i) {
+			var go = callback(f, i, section[key], level);
+			if (go === false) {
+				return true;
+			}
+			eachItem(f, key, callback, level + 1);
+		});
+	}
+
+	$scope.getSampleDoc = function(type) {
+		if (type === 'rd') {
+			return {
+				title: 'Untitled Requirements Document',
+				author: 'Anonymous',
+				created: new Date(),
+				guid: Random.id(),
+				revisions: [$scope.exampleRevision()],
+				sections: [
+					createFeature('Definitions and Conventions'),
+					createFeature('Features'),
+					createFeature('Production'),
+					createFeature('Technology'),
+					createFeature('Security')
+				],
+				flags: []
+			};
+		}
+		return {
+			title: 'Untitled Requirements Document',
+			author: 'Anonymous',
+			created: new Date(),
+			guid: Random.id(),
+			revisions: [],
+			sections: []
+		};
+	};
+
+	$scope.setWorking = function(value) {
+		$scope.working = value;
+	};
+
+	$scope.setRd = function(rd) {
+		$scope.rd = rd;
+	};
+
+	/// Section methods ///
+	$scope.insertSection = function(sectionIndex) {
+		var section = createFeature('Untitled Section');
+		$scope.rd.sections.splice(sectionIndex, 0, section);
+		Scroll.to(section.guid);
+	};
+
+	$scope.deleteSection = function(index) {
+		$scope.$apply(function() {
+			$scope.rd.sections.splice(index, 1);
+		});
+	};
+
+	/// Feature methods ///
+	$scope.deleteFeature = function(section, feature) {
+		eachItem(section, 'features', function(f, i, a) {
+			if (f.guid !== feature.guid) {
+				return true;
+			}
+			$scope.$apply(function() {
+				a.splice(i, 1);
+			});
+			return false;
+		});
+	};
+
+	$scope.insertFeature = function(features, featureIndex) {
+		if (!features) {
+			console.trace('Invalid features collection', arguments);
+			return;
+		}
+		var feature = createFeature();
+		featureIndex = featureIndex || features.length + 1;
+		features.splice(featureIndex, 0, feature);
+
+		Scroll.to(feature.guid);
+	};
+
+	/// Tag methods ///
+	var featureHasTag = $scope.featureHasTag = function(feature, tag) {
+		return feature.tags && feature.tags.indexOf(tag) >= 0;
+	};
+
+	$scope.toggleTag = function(feature, tag) {
+		if (featureHasTag(feature, tag)) {
+			removeTag(feature, tag);
+		}
+		else {
+			addTag(feature, tag);
+		}
+	};
+
+	var addTag = $scope.addTag = function(feature, tag) {
+		if (!feature.tags) {
+			feature.tags = [];
+		}
+		feature.tags.push(tag);
+	};
+
+	var removeTag = $scope.removeTag = function(feature, tag) {
+		feature.tags = feature.tags.filter(function(t) {
+			return tag !== t;
+		});
+	};
+
+	/// Flag methods ///
+	$scope.insertFlag = function(flags, type) {
+		if (!flags) {
+			return;
+		}
+		var flag = createFlag(type);
+		flags.push(flag);
+		Scroll.to(flag.guid);
+	};
+
+	$scope.deleteFlag = function(flags, index) {
+		$scope.$apply(function() {
+			flags.splice(index, 1);
+		});
+	};
+
+	$scope.moveItem = function(parentGuid, guid, newIndex) {
+		// Find the item being moved
+		var moved = findItem(guid);
+		if (!moved) {
+			console.error('Invalid moved item id', guid);
+			return;
+		}
+
+		// Get the containing object where the item was dropped
+		var parent = findItem(parentGuid || $scope.rd.guid);
+		if (!parent) {
+			console.error('Could not find drop parent in rd', parentGuid);
+			return;
+		}
+		var targetType = parent.item.guid === $scope.rd.guid ? 'sections' : 'features';
+
+		var swap = function() {
+			// Out with the old
+			moved.parent.item[moved.parent.type].splice(moved.index, 1);
+
+			// In with the new
+			parent.item[targetType].splice(newIndex, 0, moved.item);
+		};
+
+		$scope.$apply(swap);
+	};
+
+	$scope.moveFlag = function(flags, parentGuid, guid, newIndex) {
+		var currentIndex = -1;
+		var flag = null;
+		for (var i=0; i<flags.length; i++) {
+			if (flags[i].guid === guid) {
+				flag = flags[i];
+				currentIndex = i;
+				break;
+			}
+		}
+		if (currentIndex < 0) {
+			console.error('Invalid flag guid', guid);
+			return;
+		}
+		$scope.$apply(function() {
+			flags.splice(currentIndex, 1);
+			flags.splice(newIndex, 0, flag);
+		});
+	};
+
+	$scope.sortStart = function() {
+		$scope.$apply(function() {
+			$scope.sorting = true;
+		});
+	};
+
+	$scope.sortStop = function() {
+		$scope.$apply(function() {
+			$scope.sorting = false;
+		});
+	};
+
+	$scope.addFile = function(result) {
+		if (!$scope.rd.files) {
+			$scope.rd.files = {};
+		}
+		result.id = Random.id();
+		$scope.rd.files[result.id] = result;
+		addFileStyle(result);
+	};
+
+	$scope.removeFile = function(id) {
+		if (!$scope.rd.files) {return;}
+		delete $scope.rd.files[id];
+	};
+
+	$scope.formatDate = function(d) {
+		return $filter('date')(d, 'yyyy-MM-dd');
+	};
+
+	$scope.getRevision = function() {
+		var revs = $scope.rd && $scope.rd.revisions;
+		if (!revs) {
+			return 0.0;
+		}
+		return revs[revs.length - 1].revision;
+	};
+
+	$scope.exampleRevision = function() {
+		var version = 1, revision = 0.1;
+		if ($scope.rd && $scope.rd.revisions) {
+			version = $scope.rd.revisions.length + 1;
+			revision = version / 10;
+		}
+		return {
+			'id': Random.id(),
+			'version': version,
+			'revision': revision,
+			'date': $scope.formatDate(new Date()),
+			'notes': '',
+			'author': ''
+		};
+	};
+
+	$scope.addRevision = function() {
+		if (!$scope.rd.revisions) {
+			$scope.rd.revisions = [];
+		}
+		$scope.rd.revisions.push($scope.exampleRevision());
+	};
+
+	$scope.removeRevision = function(i) {
+		if (!$scope.rd.revisions) {return;}
+		$scope.rd.revisions.splice(i, 1);
+	};
+}]);
