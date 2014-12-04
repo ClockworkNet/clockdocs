c305f3562462a1ec01ef9f412e63f62f803b5810
Barrett Alexander <barrett@clockwork.net>
2014-12-04 11:17:11 -0600

    Moved rich text editing to directive because textAngular is slow with a lot of elements.

---
+++ app/scripts/directives/rte.js
@@ -0,0 +1,58 @@
+/*global $:false, angular:false */
+'use strict';
+
+angular.module('Clockdoc.Directives')
+.directive('cwRte', function() {
+	return {
+		restrict: 'A',
+		scope: {
+			model: '=cwRte',
+			titleKey: '@cwRteTitleKey',
+			contentKey: '@cwRteContentKey',
+			toggle: '@cwRteToggle'
+		},
+		link: function(scope, el) {
+			var editor = $('#modal-rte');
+			var model = scope.model;
+			var titleKey = scope.titleKey || 'title';
+			var contentKey = scope.contentKey || 'content';
+
+			var saveChanges = function() {
+				scope.$apply(function() {
+					var content = editor.find('.rte-content textarea').val();
+					model[contentKey] = content;
+				});
+			};
+
+			var openEditor = function(e) {
+				e.preventDefault();
+
+				editor.off('.cw.rte');
+				editor.on('click.cw.rte', '.rte-save', saveChanges);
+
+				editor.find('.rte-title')
+					.html(model[titleKey]);
+
+				editor.find('.rte-content [contenteditable]')
+					.html(model[contentKey])
+					.trigger('keyup');
+
+				editor.modal({keyboard: false, backdrop: 'static'});
+			};
+
+			var toggleButton = function(e, state) {
+				el.toggle(state);
+			};
+
+			el.on('click.cw.rte', openEditor);
+
+			// Hide the trigger button if a toggler has been specified
+			if (scope.toggle) {
+				var body = $(document.body);
+				body.on('focus.cw.rte', scope.toggle, toggleButton.bind(true));
+				body.on('blur.cw.rte', scope.toggle, toggleButton.bind(false));
+				el.hide();
+			}
+		}
+	};
+});
